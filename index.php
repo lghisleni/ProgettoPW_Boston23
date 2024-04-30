90285d31f2f278bcb1c25245f1c23d4d151c0bc0
lghisleni <92816061+lghisleni@users.noreply.github.com>
2024-04-30 17:17:20 +0200

    Preparazione progetto
    Scrittura della pagina di indice per accedere ai vari progetti + altra roba.

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="generator" content="AlterVista - Editor HTML"/>
+  <title>SitoPWBoston23</title>
+
+  <!-- foglio di stile -->
+  <link rel="stylesheet" href="index.css">
+
+  <!-- link per scaricare i font -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Inter:wght@300;400;700;900&family=Jersey+10&family=Jersey+15&display=swap" rel="stylesheet">
+
+</head>
+
+
+<body>
+
+  <?php
+    include 'header.html';
+    include 'footer.html';
+  ?>
+  
+  <p id="introduzione"> In questa pagina potrete trovare i progetti relativi al corso di Programmazione Web del gruppo Boston23: </p>
+  
+  <div id="contenuto">
+
+    <ul>
+      <li><a href="/ProgettoRicettario/index.php" target="_self">Progetto Ricettario</a></li>
+      <li><a href="/ProgettoRicettario/index.php" target="_self">Progetto Due</a></li>
+    </ul>
+
+  </div>
+
+</body>
+
+</html>
