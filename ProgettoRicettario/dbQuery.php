63a5f181d3b34737c51a4bd4c1f1b16a7d80f988
Roberto-Zambelli <r.zambelli3@studenti.unibg.it>
2024-05-07 16:05:45 +0200

    jfjfj

--- ProgettoRicettario/dbQuery.php
+++ ProgettoRicettario/dbQuery.php
@@ -1,6 +1,5 @@
 <?php
 function getRicettaQry($numero, $titolo, $tipo): string {
-    // Correggi la query per calcolare il numero di libri e assicurati di avere spazi appropriati
     $numeroLibri = "(SELECT Ricetta.numero AS numero1, COUNT(DISTINCT Libro.codISBN) AS nlibri1 " .
                     "FROM Ricetta " .
                     "JOIN RicettaPubblicata ON Ricetta.numero = RicettaPubblicata.numeroRicetta " . 
@@ -8,7 +7,6 @@ function getRicettaQry($numero, $titolo, $tipo): string {
                     "JOIN Libro ON Pagina.libro = Libro.codISBN " .
                     "GROUP BY Ricetta.numero) AS Ricetta1";
 
-    // Utilizza la subquery correttamente nella FROM clause
     $qry = "SELECT Ricetta.numero AS numero, Ricetta.titolo AS titolo, Ricetta.tipo AS tipo, Libro.titolo AS titololibro, Ricetta1.nlibri1 AS nlibri " .
            "FROM Ricetta " .
            "JOIN RicettaPubblicata ON Ricetta.numero = RicettaPubblicata.numeroRicetta " . 
@@ -17,7 +15,6 @@ function getRicettaQry($numero, $titolo, $tipo): string {
            "JOIN " . $numeroLibri . " ON Ricetta.numero = Ricetta1.numero1 " .
            "WHERE 1=1 ";
 
-    // Aggiungi condizioni basate sui parametri passati
     if ($numero != "") {
         $qry .= "AND Ricetta.numero = '$numero' ";
     }
@@ -72,6 +69,60 @@ function getRegioneQry($cod, $nome): string {
             "ORDER BY Regione.cod";
     return $qry;   
 }
+
+function getIngredientiQry($numeroRicetta, $numero, $ingrediente, $quantita): string {
+    $qry = "SELECT Ingrediente.numeroRicetta as ricetta, Ingrediente.numero as numero, Ingrediente.ingrediente as ingrediente, Ingrediente.quantita as quantita, Ricetta.titolo as nomericetta " . 
+            "FROM Ingrediente " . 
+            "JOIN Ricetta ON Ingrediente.numeroRicetta = Ricetta.numero " . 
+            "WHERE 1=1 ";
+    if ($numeroRicetta != "") {
+        $qry .= "AND Ingrediente.numeroRicetta = '$numeroRicetta' ";
+    }
+    if ($numero != "") {
+        $qry .= "AND Ingrediente.numero = '$numero' ";
+    }
+    if ($ingrediente != "") {
+        $qry .= "AND Ingrediente.ingrediente LIKE '%$ingrediente%' ";
+    }
+    if ($quantita != "") {
+        $qry .= "AND Ingrediente.quantita = '$quantita' ";
+    }
+
+    $qry .= "ORDER BY Ingrediente.numero";
+    return $qry; 
+}
+
+function insertIngredientiQry($conn, $numeroRicetta, $numero, $ingrediente, $quantita): string {
+    $controllo = "SELECT COUNT(*)" . 
+                "FROM Ingrediente " . 
+                "WHERE Ingrediente.ingrediente = '$ingrediente' AND Ingrediente.numeroRicetta = '$numeroRicetta'";
+                "GROUP BY Ingrediente.ingrediente, Ingrediente.numeroRicetta";
+    try {   
+        $result = $conn->query($controllo);
+    } catch(PDOException$e) {
+        echo "<p> DB Error on Query: " . $e->getMessage() . "</p>";
+        $error = true;
+    }
+    
+    if ($result < 1) {
+        $massimo = "SELECT MAX(Ingrediente.numero) " .
+                    "FROM Ingrediente " . 
+                    "WHERE Ingrediente.numero = '$numero'";
+                    "GROUP BY Ingrediente.ingrediente";
+                    "ORDER BY Ingrediente.ingrediente";
+
+        try {   
+            $result = $conn->query($controllo);
+        } catch(PDOException$e) {
+            echo "<p> DB Error on Query: " . $e->getMessage() . "</p>";
+            $error = true;
+        }
+    }
+
+    $insert = "INSERT INTO Ingrediente (numeroRicetta, numero, ingrediente, quantita) VALUES "
+
+
+}   
 
 function formattaQuery($inputString) {
     // Controlla se la stringa di input non è vuota
