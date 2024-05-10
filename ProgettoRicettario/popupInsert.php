f63ec39b789e9589cd2efed60ca78a9581469416
lghisleni <92816061+lghisleni@users.noreply.github.com>
2024-05-10 08:32:19 +0200

    rifinimento

--- ProgettoRicettario/popupInsert.php
+++ ProgettoRicettario/popupInsert.php
@@ -1,5 +1,6 @@
 <div class="popupInsert" id="popupInsert">
     <img src="imm/insert.png"></img>
+    <h2> Per inserire correttamente l'ingrediente specificare un numero ricetta valido </p>
     <form name="formInsert" id="formInsert" onsubmit="return handleFormInsert(event)" method="POST">
         <input id="numeroRicettaIns" name="nomeRicettaIns" type="text" placeholder="Inserisci il numero ricetta"/>
         <input id="nomeIngredienteIns" name="nomeIngredienteIns" type="text" placeholder="Inserisci il nome"/>
